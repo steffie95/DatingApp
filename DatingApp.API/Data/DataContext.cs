623e0206cba1f01f26130cff8a54c5b760604509
steffie95 <60145379+steffie95@users.noreply.github.com>
2020-02-24 00:49:23 -0500

    Added Register Method

--- DatingApp.API/Data/DataContext.cs
+++ DatingApp.API/Data/DataContext.cs
@@ -9,7 +9,7 @@ namespace DatingApp.API.Data
         public DataContext(DbContextOptions<DataContext>options) : base (options){}
 
         public DbSet<Value> Values {get; set;}
-
+        public DbSet<User> Users{get; set;}
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder){
             optionsBuilder.UseSqlServer(@"Server=DESKTOP-6EV5SNM\LOCALHOST;Database=ValuesDB;Trusted_Connection=True;");
         }
