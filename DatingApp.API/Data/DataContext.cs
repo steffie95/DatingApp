cdeb492a9e93d9b42713e02450746ae5286070bd
steffie95 <60145379+steffie95@users.noreply.github.com>
2020-02-23 23:00:10 -0500

    Initial Commit

---
+++ DatingApp.API/Data/DataContext.cs
@@ -0,0 +1,17 @@
+
+using DatingApp.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext>options) : base (options){}
+
+        public DbSet<Value> Values {get; set;}
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder){
+            optionsBuilder.UseSqlServer(@"Server=DESKTOP-6EV5SNM\LOCALHOST;Database=ValuesDB;Trusted_Connection=True;");
+        }
+    }
+}
---
+++ DatingApp.API/Migrations/20200224011616_SecondMigration.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DatingApp.API.Migrations
+{
+    public partial class SecondMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_MyProperty",
+                table: "MyProperty");
+
+            migrationBuilder.RenameTable(
+                name: "MyProperty",
+                newName: "Values");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Values",
+                table: "Values",
+                column: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Values",
+                table: "Values");
+
+            migrationBuilder.RenameTable(
+                name: "Values",
+                newName: "MyProperty");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_MyProperty",
+                table: "MyProperty",
+                column: "Id");
+        }
+    }
+}
