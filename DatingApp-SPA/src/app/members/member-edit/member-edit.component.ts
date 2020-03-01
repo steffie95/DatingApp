dff481f2c9ae8640d79f76244199be5127c40464
steffie95 <60145379+steffie95@users.noreply.github.com>
2020-03-01 11:41:37 -0500

    Added some directives to the member-edit component's editForm.

--- DatingApp-SPA/src/app/members/member-edit/member-edit.component.ts
+++ DatingApp-SPA/src/app/members/member-edit/member-edit.component.ts
@@ -1,6 +1,8 @@
+import { AlertifyService } from './../../services/alertify.service';
 import { User } from './../../_models/user';
 import { ActivatedRoute } from '@angular/router';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 
 @Component({
@@ -9,13 +11,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./member-edit.component.css']
 })
 export class MemberEditComponent implements OnInit {
-  user:User;
-  constructor(private route: ActivatedRoute) { }
+  @ViewChild('editForm', {static: true}) editForm: NgForm;
+  user: User;
+  constructor(private route: ActivatedRoute, private alertify: AlertifyService) { }
 
   ngOnInit() {
   this.route.data.subscribe( data => {
     this.user = data['user'];
   });
   }
+
+  updateUser() {
+    console.log(this.user);
+    this.alertify.success('Profile updated successfully');
+    this.editForm.resetForm(this.user);
+  }
+
 
 }
