bff7bd134c4bc86e93e01b0d23d1e704d8863c44
Stefanie Adrianzen <stefanie.adrianzen@gmail.com>
2020-03-02 12:40:16 -0500

    Added PhotosController for uploading photos to Cloudinary.

---
+++ DatingApp.API/Dtos/PhotoForReturnDto.cs
@@ -0,0 +1,22 @@
+using System;
+using Microsoft.AspNetCore.Http;
+
+namespace DatingApp.API.Dtos
+{
+    public class PhotoForReturnDto
+    {
+       public int id {get; set;}
+
+        public string Url {get; set;}
+    
+        public string Description {get; set;}
+
+        public DateTime DateAdded {get; set;}
+
+        public bool IsMain {get; set;}
+
+        public string PublicId {get; set;}
+
+
+    }
+}
