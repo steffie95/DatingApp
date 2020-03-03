33a86cf530d5eae617f6b52bd08de63d3d0109dd
steffie95 <60145379+steffie95@users.noreply.github.com>
2020-03-03 16:25:56 -0500

    Updated the API to handle the additional user information from the register form.

--- DatingApp.API/Dtos/UserForRegisterDto.cs
+++ DatingApp.API/Dtos/UserForRegisterDto.cs
@@ -1,3 +1,4 @@
+using System;
 using System.ComponentModel.DataAnnotations;
 
 namespace DatingApp.API.Dtos
@@ -11,5 +12,23 @@ namespace DatingApp.API.Dtos
         [DataType(DataType.Password)]
         [StringLength(20, MinimumLength = 8, ErrorMessage = "Password must be between 8 and 20 characters long.")]
         public string Password {get; set;}
+        [Required]
+        public string Gender {get; set;}
+        [Required]
+        public string KnownAs {get; set;}
+        [Required]
+        public DateTime DateOfBirth {get; set;}
+        [Required]
+        public string City {get; set;}
+        [Required]
+        public string Country {get; set;}
+        public DateTime Created {get; set;}
+        public DateTime LastActive {get; set;}
+        
+        public UserForRegisterDto()
+        {
+            Created = DateTime.Now;
+            LastActive = DateTime.Now;
+        }
     }
 }
